60f2aca72d5c1e671866acd139e547fbb2430755
Mattias Elmers <mattias.elmers@gmail.com>
2024-01-10 13:56:47 +0200

    Update iseseisev töö 3.py

--- iseseisev töö 3.py
+++ iseseisev töö 3.py
@@ -5,6 +5,42 @@ import random
 
 
 
+siiamidagi = input("Sisesta faili nimi: ")
+fail = open(siiamidagi, encoding="UTF-8")
+print("muusikapalade valik:")
+nr = 1
+for i in fail:
+    print(f"{nr}. {i}" ,end="")
+    nr += 1
+valik = int(input("Vali laulu arv: "))
+
+fail.seek(0) #keri faili algusesse
+nr = 1
+for i in fail:
+    if nr == valik:
+        print(i)
+    nr += 1
+
+input()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+input()
+
+#3.2
+
+
 fail = open("konto.txt")
 for i in fail:
     if float(i) > 0:
