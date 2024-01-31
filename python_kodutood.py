232a21823bc4c4fcc52a934cce1b6b3100cc67ab
Mattias Elmers <mattias.elmers@gmail.com>
2024-01-31 14:36:06 +0200

    Update python_kodutood.py

--- python_kodutood.py
+++ python_kodutood.py
@@ -3,6 +3,90 @@ import random
 #16.01.24
 #Kodutöö
 
+
+#11. Ülesanne
+
+
+tähestik = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k","l","m","n","o","p","q","r","s","t","u","v","w","x", "y", "z", "ä", "ö", "ü", "õ"]
+
+def salasta(tavakiri, sala_võti):
+    salakiri = ""
+    for i in tavakiri:
+        if i.isalpha():
+            i = i.lower()
+            i_indeks = tähestik.index(i)
+            uus_indeks = (i_indeks + sala_võti) % 26
+            sala_täht = tähestik[uus_indeks].upper()
+        else:
+            sala_täht = i
+
+        salakiri += sala_täht
+
+    print("sinu salakiri on: " + salakiri)
+
+
+
+
+#9. Ülesanne
+
+def emaili_kontroll(email):
+    email = input("Sisesta email: ")
+    if "@" in email:
+        nimi = email.split('.')[0]
+        domeen = email.split('@')[1]
+        print(f"Tere {nimi}, sinu email on {email} ja domeeniks on {domeen}.")
+    else:
+        print("Sisestatud email on vale")
+
+print(emaili_kontroll("email"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#7. Ülesanne
+
+def arvutus(küsimus):
+    #küsib küsimuse
+    küsimus = input("Kas soovid arvutada EUR --> EEK või EEK --> EUR? (EUR --> EEK = 1, EEK --> EUR = 2): ")
+    #kui vastus on 1, siis arvutab eurod kroonideks
+    if küsimus == "1":
+        euro = int(input("Sisesta eurode arv: "))
+        print(f"{euro} eurot on {euro * 15.6466} krooni.")
+        #kui vastus on 2, siis arvutab kroonid eurodeks
+    elif küsimus == "2":
+        kroon = int(input("Sisesta kroonide arv: "))
+        print(f"{kroon} krooni on {kroon / 15.6466} eurot.")
+        #kui vastus on midagi muud, siis ütleb, et sellist valikut ei ole
+    else:
+        print("sellist valikut ei ole! (valige palun kas 1 või 2)")
+
+
+
+print(arvutus("küsimus"))
+
+
+
+
+
+
+
+
+
+
+input()
+
 #5. Ülesanne
 
 ostunimekiri = []
